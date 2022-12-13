7cc88ad0a81b9b6ffe9893a97a65b01cabe4a575
Jesse Songok <jessesongok@gmail.com>
2022-12-13 13:03:08 +0300

    Approvals List logic updated

--- resources/views/requisition/approval/index.blade.php
+++ resources/views/requisition/approval/index.blade.php
@@ -44,7 +44,7 @@
 
                                 </table>
                             @else
-                                <h2 class="mt-3">
+                                <h2 class="mt-6">
                                     <span class="alert alert-warning alert-heading">No Requisitions Posted for Approval </span>
                                 </h2>
                             @endif
