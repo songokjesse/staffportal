d6027592f5ca93a71f7d13d308104566860584fa
Jesse Songok <jessesongok@gmail.com>
2022-12-13 12:59:34 +0300

    Approvals List logic

--- resources/views/requisition/approval/index.blade.php
+++ resources/views/requisition/approval/index.blade.php
@@ -13,35 +13,39 @@
                         @endif
                         <h2>{{ __('Requisitions Approval') }}</h2>
 
-                        <table class="table table-responsive table-striped table-bordered mt-3">
-                            <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Description</th>
-                                <th>Department</th>
-                                <th>Total</th>
-                                <th>Initiated By</th>
-                                <th>Status</th>
-                                <th></th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($requisitions as $requisition)
-                                <tr>
-                                    <td>{{$loop->iteration}}</td>
-                                    <td>{{$requisition->description}}</td>
-                                    <td>{{$requisition->department->name}}</td>
-                                    <td>Ksh {{$requisition->total}}</td>
-                                    <td>{{$requisition->user->name}}</td>
-                                    <td><span class="badge text-bg-warning"><i class="bi bi-arrow-repeat"></i> {{$requisition->status}}</span></td>
-                                    <td>
-                                        <a href="{{route('requisitions.show', $requisition->id)}}" class="btn btn-primary btn-sm"><i class="bi bi-list-check"></i> Approvals </a>
-                                    </td>
-                                </tr>
-                            @endforeach
-                            </tbody>
+                            @if (count($requisitions) > 0)
+                                <table class="table table-responsive table-striped table-bordered mt-3">
+                                    <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Description</th>
+                                        <th>Department</th>
+                                        <th>Total</th>
+                                        <th>Initiated By</th>
+                                        <th>Status</th>
+                                        <th></th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($requisitions as $requisition)
+                                        <tr>
+                                            <td>{{$loop->iteration}}</td>
+                                            <td>{{$requisition->description}}</td>
+                                            <td>{{$requisition->department->name}}</td>
+                                            <td>Ksh {{$requisition->total}}</td>
+                                            <td>{{$requisition->user->name}}</td>
+                                            <td><span class="badge text-bg-warning"><i class="bi bi-arrow-repeat"></i> {{$requisition->status}}</span></td>
+                                            <td>
+                                                <a href="{{route('requisitions.show', $requisition->id)}}" class="btn btn-primary btn-sm"><i class="bi bi-list-check"></i> Approvals </a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
 
-                        </table>
+                                </table>
+                            @else
+                                I don't have any records!
+                            @endif
                     </div>
                 </div>
             </div>
