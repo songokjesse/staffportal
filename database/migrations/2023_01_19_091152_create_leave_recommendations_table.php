3fc23e7f3f6124c7fe9c7c35db41d9b19dfb70d9
Jesse Songok <jessesongok@gmail.com>
2023-01-30 11:43:21 +0300

    Fix migration

--- database/migrations/2023_01_19_091152_create_leave_recommendations_table.php
+++ database/migrations/2023_01_19_091152_create_leave_recommendations_table.php
@@ -17,8 +17,8 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('leave_application_id');
-            $table->boolean('recommendation')->default('False');
-            $table->boolean('not_recommendation')->default('False');
+            $table->boolean('recommendation')->default(false);
+            $table->boolean('not_recommendation')->default(false);
             $table->text('comments')->nullable();
             $table->foreign('user_id')->references('id')->on('users');
             $table->foreign('leave_application_id')->references('id')->on('leave_applications');
