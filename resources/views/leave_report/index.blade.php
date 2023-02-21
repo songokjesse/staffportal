9fc8ba69e10b44ff37012ae395f7532c100a338b
Jesse Songok <jessesongok@gmail.com>
2023-02-21 11:47:19 +0300

    Leave Report UI

--- resources/views/leave_report/index.blade.php
+++ resources/views/leave_report/index.blade.php
@@ -15,6 +15,32 @@
 
                         <a href="{{route('leave_calendar')}}" class="btn btn-primary btn-sm"> <i class="bi bi-calendar"></i> Calendar View</a>
 
+
+                            <hr>
+                            <div class="row mt-3">
+                                <div class="col-xl-3 col-md-6 mb-4">
+                                    <div class="card border-left-warning shadow h-100 py-2">
+                                        <div class="card-body">
+                                            <div class="row no-gutters align-items-center">
+                                                <div class="col mr-2">
+                                                    <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                                        On Leave
+                                                    </div>
+                                                    <div class="h5 mb-0 font-weight-bold text-gray-800">0</div>
+                                                </div>
+                                                <div class="col-auto">
+                                                    <i class="fas fa-comments fa-2x text-gray-300"></i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <h3 class="mt-2">Leave Status</h3>
+                            <hr>
+
                             <!-- Content Row -->
                             <div class="row mt-3">
 
@@ -96,27 +122,6 @@
                             </div>
 
                             <!-- Content Row -->
-                        <hr>
-                        <div class="row mt-3">
-                            <div class="col-xl-3 col-md-6 mb-4">
-                                <div class="card border-left-warning shadow h-100 py-2">
-                                    <div class="card-body">
-                                        <div class="row no-gutters align-items-center">
-                                            <div class="col mr-2">
-                                                <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                                    On Leave
-                                                </div>
-                                                <div class="h5 mb-0 font-weight-bold text-gray-800">0</div>
-                                            </div>
-                                            <div class="col-auto">
-                                                <i class="fas fa-comments fa-2x text-gray-300"></i>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
 
                     </div>
                 </div>
