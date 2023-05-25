7d4149fa57fc111e518b849ec50c169d3e712f56
Jesse Songok <jessesongok@gmail.com>
2023-05-25 13:59:57 +0300

    Leave Approvals refactor

--- app/Http/Controllers/LeaveApprovalController.php
+++ app/Http/Controllers/LeaveApprovalController.php
@@ -24,6 +24,7 @@ class LeaveApprovalController extends Controller
             ->join('leave_approvals', 'leave_applications.id', '=', 'leave_approvals.leave_application_id')
             ->join('users', 'leave_applications.user_id', '=', 'users.id')
             ->join('leave_categories', 'leave_applications.leave_categories_id', '=', 'leave_categories.id')
+            ->join('assigned_duties', 'leave_applications.id', '=', 'assigned_duties.leave_application_id')
             ->where('leave_approvals.user_id','=' ,Auth::user()->id)
             ->where('leave_approvals.approved', '=',false)
             ->where('leave_approvals.not_approved', '=',false)
@@ -34,7 +35,7 @@ class LeaveApprovalController extends Controller
                 'leave_applications.end_date',
                 'leave_applications.days',
                 DB::raw("(Select name from users where id = leave_applications.user_id) as applicant_name"),
-                DB::raw("(Select name from users where id = leave_applications.duties_by_user_id) as left_in_charge"),
+                DB::raw("(Select name from users where id = assigned_duties.user_id) as left_in_charge"),
             )
             ->get();
         return view('leave_approval.index', compact('approvals'));
