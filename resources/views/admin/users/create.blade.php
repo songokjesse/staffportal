551898f83618789a6f6b15b3f2373c704cb81a75
Jesse Songok <jessesongok@gmail.com>
2023-09-04 12:07:30 +0300

    Refactor: User Create Search Select

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -21,7 +21,7 @@
                                 @method('POST')
                                 <div class="mb-3">
                                     <label for="name" class="form-label">Job Title</label>
-                                   <select name="job_title_id" class="form-control" required>
+                                   <select name="job_title_id" class="form-control job_title" required>
                                        <option selected disabled> Select Job Title</option>
                                        @foreach($job_titles as $title)
                                            <option value="{{$title->id}}">{{$title->name}}</option>
@@ -105,3 +105,16 @@
     </div>
 @endsection
 
+@section('scripts')
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.rtl.min.css" />    <script>
+        $(document).ready(function () {
+
+            $('.job_title').select2({
+                theme: "bootstrap-5"
+            });
+        });
+
+    </script>
